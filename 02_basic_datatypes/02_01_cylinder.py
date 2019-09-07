c4a14611ec8ba355a0f2c8417c1a79adf456cc30
Firas <Firasabdulhasain@gmail.com>
2019-09-07 22:29:19 +0200

    Commit assignment

--- 02_basic_datatypes/02_01_cylinder.py
+++ 02_basic_datatypes/02_01_cylinder.py
@@ -4,3 +4,12 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+
+#  cylinder radius (r) = 3.14 , height (h) = 5.
+# volume (V) of a cylinder is V = π * (radius ** 2) * height
+# π = 3.14
+print(3.14 * (3.14 **2) * 5) # Volume of the cylinder
+
+# surface area (A) of a cylinder is A = 2 * π * r * h + 2 * π * (r ** 2)
+# π = 3.14
+print(2 * 3.14 * 3.14 * 5 + 2 * 3.14 * (3.14 ** 2)) # is the surface area (A) of the cylinder
