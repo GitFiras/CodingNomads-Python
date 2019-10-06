a1bf0a9b85aa956e5450fdc8961529ad6f520872
Firas <Firasabdulhasain@gmail.com>
2019-10-06 21:24:36 +0200

    Commit assignment

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,8 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def hello(*greeting):
+    for item in greeting:
+        print(greeting)
+hello('hi, hello')
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,7 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+def destinations(**holidays):
+    for country,city in holidays.items():
+        print(country,city)
+destinations(Europe='France', Asia='Indonesia')
