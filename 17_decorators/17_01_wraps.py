3c2b96ec9b500ee5ee360148820bee73b0725ae6
Firas <Firasabdulhasain@gmail.com>
2019-10-07 00:50:55 +0200

    Commit assignment

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -2,3 +2,15 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+def text(name):
+   return "Hello, {0}".format(name)
+
+def p_decorate(func):
+   def wrapper(name):
+       return "<p>{0}</p>".format(func(name))
+   return wrapper
+
+greeting_text = p_decorate(text)
+
+print(greeting_text("John"))
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,19 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def tags(tag_name):
+    def tags_decorator(func):
+        def func_wrapper(name):
+            return "<{0}>{1}</{0}>".format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
+
+@tags("x")
+def text(name):
+   return "Hello, {0}".format(name)
+
+greeting_text = text
+
+print(greeting_text("John"))
+
